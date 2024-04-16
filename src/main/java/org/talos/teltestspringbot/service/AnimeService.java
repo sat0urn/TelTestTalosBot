318ef7b97f48f5d832db6a3717f1b469f84f0e46
Aslan <nissanfordo03@gmail.com>
2024-04-16 23:59:15 +0500

    change

--- src/main/java/org/talos/teltestspringbot/service/AnimeService.java
+++ src/main/java/org/talos/teltestspringbot/service/AnimeService.java
@@ -63,8 +63,6 @@ public class AnimeService {
         System.out.println("1");
         uc.setRequestMethod("GET");
         uc.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0)");
-        uc.setRequestProperty("Accept-Language", "en-US,en,ru-RU,ru;q=0.5");
-        uc.setRequestProperty("Accept", "text/html");
         System.out.println("2");
 
         BufferedReader in = new BufferedReader(new InputStreamReader(uc.getInputStream(), StandardCharsets.UTF_8));
